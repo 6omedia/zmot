0fc563c85c68a4edbf47cae077682fb3dfaa5bb0
Mike <mike@6omedia.co.uk>
2017-01-30 12:52:34 +0000

    hjkhkjhj

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.set('views', __dirname + '/templates');
 mongoose.Promise = global.Promise;
 // mongodb connection
 
-mongoose.connect("mongodb://jason@6omedia.co.uk:6DMedia89!@ds033259.mlab.com:33259/heroku_pcdrm19z");
+mongoose.connect("mongodb://6omedia:6DMedia89!@ds033259.mlab.com:33259/heroku_pcdrm19z");
 
 // mongoose.connect("mongodb://localhost:27017/zmot");
 
